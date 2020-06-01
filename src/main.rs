43ac0171ac8a6f113f0a743119b6ad6bc56b309e
peccu <peccul@gmail.com>
2020-06-01 12:28:36 +0900

    implement simple totp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::time::SystemTime;
+use otp::make_totp;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let secret = &args[1];
+    print!("{}", make_totp(secret, 30, 0).unwrap());
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => eprintln!("valid in {} seconds.", 30 - n.as_secs() % 30),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
 }
